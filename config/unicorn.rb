9e08497bdabc245505aaebb040189790fdc4ea8d
ryota-1231 <r.aoyagi@miracleave.co.jp>
2021-08-15 13:51:02 +0900

    修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../', __FILE__)
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+working_directory "/var/www/#{app_path}"
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "/var/www/#{app_path}/tmp/pids/unicorn.pid"
 #ポート番号を指定
